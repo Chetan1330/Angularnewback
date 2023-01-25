801d406c08a78f292cbf152600150a04892614ff
Chetan1330 <41874307+Chetan1330@users.noreply.github.com>
2023-01-25 13:13:48 +0530

    Update settings.py

--- Djangoapis/settings.py
+++ Djangoapis/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Djangoapis.wsgi.application'
 
-CSRF_TRUSTED_ORIGINS = ['https://django-server-production-85a6.up.railway.app', 'https://web-production-24d2.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://django-server-production-85a6.up.railway.app', 'https://angularnewback-production.up.railway.app']
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
