041ebaaaa587af3cbc4a62ffe2edd77eb2a55fee
Chetan1330 <41874307+Chetan1330@users.noreply.github.com>
2023-02-05 13:35:58 +0530

    Update admin.py

--- apis/admin.py
+++ apis/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import SecondStock,CustomUser,FirstStock,Scenario,ScenarioSolution
+from .models import SecondStock,CustomUser,FirstStock,Scenario,ScenarioSolution,ScenarioUser
 # Register your models here.
 admin.site.register(SecondStock)
 admin.site.register(FirstStock)
 admin.site.register(CustomUser)
 admin.site.register(Scenario)
 admin.site.register(ScenarioSolution)
+admin.site.register(ScenarioUser)
